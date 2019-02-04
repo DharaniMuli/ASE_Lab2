f48faf5517e66bbe5acd978120d8e09d50eeeaed
DharaniMuli <dharanimuli559@gmail.com>
2019-02-04 00:24:11 -0500

    Merge branch 'master' of D:\ASE\AseLab2 with conflicts.

--- DPWeather/src/app/app.module.ts
+++ DPWeather/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule} from "@angular/common/http";
-import { FormsModule} from "@angular/forms";
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 
-import {DataService} from "./data.service";
+import {DataService} from './data.service';
 
 import { NavbarComponent } from './template/navbar/navbar.component';
 import { TemperatureComponent } from './temperature/temperature.component';
